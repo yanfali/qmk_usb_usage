af832fdbec3cc881f010be4717e24892020ddc5d
Yan-Fa Li <yanfa.li@fireeye.com>
2020-03-17 14:11:45 -0700

    Add fuzzy searching

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <h2>QMK USB VID/PID Usage</h2>
-    {{ this.rowData.length }} keyboards, {{ this.vids.length }} vids,
-    {{ this.pids.length }} pids
+    <div class="top-bar">
+      <div class="left-top">
+        <input
+          class="input-filter"
+          type="text"
+          v-model="filter"
+          placeholder="filter on content"
+        />
+      </div>
+      <div class="right-top">
+        {{ this._rowData.length }} keyboards, {{ this.vids.length }} VIDs,
+        {{ this.pids.length }} PIDs
+      </div>
+    </div>
     <ag-grid-vue
       style="width: 100vw; height: 100vh;"
       class="ag-theme-balham-dark"
       :columnDefs="columnDefs"
-      :rowData="rowData"
+      :defaultColDef="defaultColDef"
+      :rowData="_rowData"
+      :sizeColumnsToFit="true"
     >
     </ag-grid-vue>
   </div>
@@ -16,6 +30,7 @@
 <script>
 import { AgGridVue } from "ag-grid-vue";
 import axios from "axios";
+import Fuse from "fuse.js";
 
 export default {
   name: "HelloWorld",
@@ -24,18 +39,51 @@ export default {
   },
   data() {
     return {
+      defaultColDef: {
+        sortable: true,
+        resizable: true
+      },
       columnDefs: [
-        { headerName: "Keyboard", field: "keyboard", sortable: true },
-        { headerName: "VendorID", field: "vendor_id", sortable: true },
-        { headerName: "ProductID", field: "product_id", sortable: true },
-        { headerName: "Device ver", field: "device_ver", sortable: true },
-        { headerName: "Manu", field: "manufacturer", sortable: true },
-        { headerName: "Desc", field: "description", sortable: true }
+        {
+          headerName: "Keyboard",
+          field: "keyboard"
+        },
+        {
+          headerName: "VendorID",
+          field: "vendor_id"
+        },
+        {
+          headerName: "ProductID",
+          field: "product_id"
+        },
+        {
+          headerName: "Device ver",
+          field: "device_ver"
+        },
+        {
+          headerName: "Manu",
+          field: "manufacturer"
+        },
+        {
+          headerName: "Desc",
+          field: "description"
+        }
       ],
       rowData: [],
       vids: [],
-      pids: []
+      pids: [],
+      filter: "",
+      fuse: undefined
     };
+  },
+  computed: {
+    _rowData() {
+      if (this.filter === "") {
+        return this.rowData;
+      } else {
+        return this.fuse.search(this.filter).map(r => r.item);
+      }
+    }
   },
   beforeMount() {
     axios.get("https://api.qmk.fm/v1/usb").then(resp => {
@@ -61,6 +109,19 @@ export default {
         }, rowData);
         rowData = rowData.flat(3);
         this.rowData = rowData;
+        this.fuse = new Fuse(rowData, {
+          keys: [
+            "keyboard",
+            "vendor_id",
+            "product_id",
+            "device_ver",
+            "description",
+            "manufacturer"
+          ],
+          minMatchCharLength: 3,
+          distance: 30,
+          threshold: 0.4
+        });
       }
     });
   }
@@ -71,4 +132,19 @@ export default {
 <style lang="scss">
 @import "../../node_modules/ag-grid-community/dist/styles/ag-grid.css";
 @import "../../node_modules/ag-grid-community/dist/styles/ag-theme-balham-dark.css";
+.top-bar {
+  display: grid;
+  grid-template: 1fr / 1fr 1fr;
+}
+.left-top {
+  text-align: left;
+  width: 100%;
+}
+.right-top {
+  text-align: right;
+  width: 100%;
+}
+.input-filter {
+  width: 80%;
+}
 </style>
